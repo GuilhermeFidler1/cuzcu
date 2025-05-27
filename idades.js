4d8ee612751ffbe2ed96619b841b1018665e329b
GuilhermeFidler1 <guilherme_fidler@estudante.sesisenai.org.br>
2025-05-27 17:08:31 -0300

    Add files via upload

---
+++ idades.js
@@ -0,0 +1,10 @@
+// ler a idade do usuario 
+// se a idade 18 ou mais, dizer que é maior
+//senão, diz que é menor
+
+let idade = prompt("Qual sua idade?")
+if(idade>=18){
+    alert("Você é maior")
+}else{
+    alert("Você é menor de idade.")
+}
